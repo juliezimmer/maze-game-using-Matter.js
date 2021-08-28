0ece983e5332da213765389f61c268a75798c49c
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-08-27 21:51:48 -0500

    added code to update horizontal and vertical wall values

--- index.js
+++ index.js
@@ -38,6 +38,24 @@ const walls = [
 World.add(world, walls);
 
 // Maze generation //
+// randomizes neighboring cells for maze //
+// This will take an array and randomly re-order all the elements inside of it //
+const shuffle = (arr) => {
+   let counter = arr.length;
+   // runs while counter is greater than zero //
+   while (counter > 0) {
+      // get random index inside the array
+      const index = Math.floor(Math.random() * counter); 
+      // decrement counter //
+      counter--;
+      const temp = arr[counter];
+      // update value at index[counter]
+      arr[counter] = arr[index];
+      arr[index] = temp;
+   }
+   return arr;
+ };
+
 // define grid array //
 const grid = Array(cells) // creates 3 inner arrays
    .fill(null)
@@ -56,3 +74,59 @@ const horizontals = Array(cells - 1) // creates 2 inner arrays
    .fill(null)
       .map(() => Array(cells).fill(false));
 console.log(horizontals);
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+console.log(startRow, startColumn);
+
+// function that iterates through maze //
+const stepThroughCell = (row, column) => {
+   // if cell at [row, column] has been visited, then return 
+   if(grid[row][column]){
+      return;  
+   }
+   // mark cell as visited
+   grid[row][column] = true;
+   
+// Assemble randomly-ordered list of neighbors
+const neighbors = shuffle([
+   [row - 1, column, 'up'], // above
+   [row, column + 1, 'right'], // right
+   [row + 1, column, 'down'], // below
+   [row, column - 1, 'left'] // left
+]);
+   
+// For each neighbor...iterate over
+for(let neighbor of neighbors){
+   const [nextRow, nextColumn, direction] = neighbor;
+      
+      // See if neighbor is out of bounds //
+      if (
+         nextRow < 0 || 
+         nextRow >= cells || 
+         nextColumn < 0 || 
+         nextColumn >= cells
+      ){
+        continue;    
+      }
+      // if neighbor has been visited, continue to next neighbor //
+      if (grid[nextRow][nextColumn]){
+         continue;
+      }
+      // remove wall from horizontals or verticals array
+      // verticals
+      if(direction === 'left'){
+         verticals[row][column - 1] = true;
+      } else if (direction === 'right'){
+         verticals[row][column] = true;
+      } else if (direction === 'up'){ 
+         horizontals[row - 1][column] = true;
+      } else if(direction === 'down'){
+         horizontals[row][column] = true;
+      }
+   } 
+   // visit that next cell (call function again with row and column trying to visit)
+};
+
+stepThroughCell(startRow, startColumn); // yields a random cell with two coordinates using indicies of grid array //
